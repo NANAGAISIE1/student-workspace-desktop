267c4585a3502c6931f49109b08ea14a8bd13e8d
Nana Gaisie <nabgaisie@outlook.com>
2024-12-10 14:12:13 +0000

    chore: Add initial project files and configuration for Tauri, React, and TypeScript

--- old/convexApi1725891977443.ts
+++
@@ -1,8 +0,0 @@
-import { FunctionReference, anyApi } from "convex/server";
-import { GenericId as Id } from "convex/values";
-
-export const api: PublicApiType = anyApi as unknown as PublicApiType;
-export const internal: InternalApiType = anyApi as unknown as InternalApiType;
-
-export type PublicApiType = {};
-export type InternalApiType = {};
--- old/src/types/templates.ts
+++
@@ -1,9 +0,0 @@
-export type Template = {
-  title: string;
-  type: "notes" | "research" | "site" | "gettingStarted";
-  emoji: string;
-  pageType: "page" | "todo" | "calendar";
-  content: string;
-};
-
-export type Content = MyPartialBlock[];
